300b25fd4a8c3fd609b11e1cfec875e44a27217f
Can Sahin <cann2005@gmail.com>
2019-01-27 16:46:10 +0300

    not found page added

--- app/containers/NotFoundPage/index.tsx
+++ app/containers/NotFoundPage/index.tsx
@@ -1,25 +1,45 @@
-/**
- * NotFoundPage
- *
- * This is the page we show when the user visits a url that doesn't have a route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
 
-import messages from './messages';
+import TabPanel from 'components/TabPanel';
+import Footer from 'components/Footer';
+import styled from 'styles/styled-components';
 
 export default class NotFound extends React.PureComponent {
   public render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <StyledTabPanel>
+        <Wrapper>
+          <Header big> OOPS!</Header>
+          <Header> PAGE NOT FOUND</Header>
+        </Wrapper>
+        <Footer />
+      </StyledTabPanel>
     );
   }
 }
+
+const StyledTabPanel = styled(TabPanel)`
+  padding: 0px;
+  justify-content: space-between;
+`;
+const Wrapper = styled.div`
+  /* background-color: ${props => props.theme.componentBackgroundSecondary}; */
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  align-content: center;
+  padding: 64px 0px;
+`;
+
+interface HeaderProps {
+  big?: boolean;
+}
+const Header = styled<HeaderProps, 'h1'>('h1')`
+  font-size: ${props => (props.big ? '4em' : '2em')};
+  margin-bottom: 32px;
+  color: ${props => props.theme.textPrimary};
+`;
