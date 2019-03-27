aec6b397b43594734bfd463ea23276d87e00f738
Can Sahin <cann2005@gmail.com>
2019-03-27 16:30:42 +0300

    favicon changed

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -143,11 +143,11 @@ module.exports = require('./webpack.base.babel')({
       ios: true,
       icons: [
         {
-          src: path.resolve('app/images/isa-sport.png'),
+          src: path.resolve('app/images/android-chrome-512x512.png'),
           sizes: [72, 96, 128, 144, 192, 384, 512],
         },
         {
-          src: path.resolve('app/images/isa-sport.png'),
+          src: path.resolve('app/images/apple-touch-icon.png'),
           sizes: [120, 152, 167, 180],
           ios: true,
         },
